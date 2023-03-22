005dcb276f8c0fa0a050df1aa69d56d0e2b5983a
MedOK99 <127406946+MedOK99@users.noreply.github.com>
2023-03-22 12:52:52 +0300

    Example004_Div деление

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Папка 004
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
