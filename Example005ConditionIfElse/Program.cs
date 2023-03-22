0ffc4bd4978ac2e2cead19d6d172581cac7bd852
MedOK99 <127406946+MedOK99@users.noreply.github.com>
2023-03-22 20:33:21 +0300

    Программа приветствия по избранным именам

---
+++ Example005ConditionIfElse/Program.cs
@@ -0,0 +1,34 @@
+﻿// 
+Console.WriteLine("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "алексей")
+{
+    Console.WriteLine("Приветствую и повинуюсь, мой ПОВЕЛИТЕЛЬ!");
+}
+
+if(username.ToLower() == "олеся")
+{
+    Console.WriteLine("Счастлив приветствовать тебя, моя Богиня! Слушаю и повинуюсь!");
+}
+
+if(username.ToLower() == "таня")
+{
+    Console.WriteLine("О да! Это Таня! Здравия желаю тебе, моя Богиня!");
+}
+
+if(username.ToLower() == "миша")
+{
+    Console.WriteLine("ФИГАСЕ, это же МИША!!! Привет ШАЛУН!!!");
+}
+
+if(username.ToLower() == "соня")
+{
+    Console.WriteLine("Вот это да! Это же Соня! Привет ПРИНЦЕССА!");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
