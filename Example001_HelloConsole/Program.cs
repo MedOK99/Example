effa0fedb2c2c271abd80a4fffc4354033dc58b4
MedOK99 <127406946+MedOK99@users.noreply.github.com>
2023-03-23 14:04:34 +0300

    обавил данные задачи про друзей с собакой

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,2 +1,23 @@
-﻿// Пробую язык C#
-Console.WriteLine("2 Hello, World!");
+﻿// Пробую язык C#endregion
+
+int count = 0;
+int distance = 10000;
+int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int DogSpeed = 5;
+int Friend = 2;
+
+while (distance > 10)
+  if (Friend == 1)
+    int Time = distance / (FirstFriendSpeed + DogSpeed);
+    int Friend = 2;
+
+  else
+    int Time = distance / (SecondFriendSpeed + DogSpeed);
+    int Friend = 1; 
+
+distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * Time;
+count++;
+Console.WriteLine("count");
+
+
